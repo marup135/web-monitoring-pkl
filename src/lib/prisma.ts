052a313edd0b983fc2edecf8821e24319002fd3c
Marup <marup113355@gmail.com>
2026-07-09 14:33:41 +0700

    new

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,21 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-const dbUrl = process.env.DATABASE_URL || 'mysql://root:@localhost:3306/minitor_pkl';
-
-const adapter = new PrismaMariaDb(dbUrl);
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
